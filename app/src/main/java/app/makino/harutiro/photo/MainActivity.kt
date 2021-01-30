044a82f98802c3d82ae7434acf7dc976656b3169
harutiro <harutiro2027@outlook.jp>
2021-01-30 14:01:32 +0900

    add: uriデータを引き出し出力

--- app/src/main/java/app/makino/harutiro/photo/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/photo/MainActivity.kt
@@ -1,8 +1,10 @@
 package app.makino.harutiro.photo
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
@@ -10,11 +12,16 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     //インテントの戻るいちを指定
     val REQUEST_PREVIEW = 1
+    val REQUEST_PICTURE = 2
+
+    lateinit var currentPhotoUri :Uri
 
     var imageView:ImageView? = null
 
@@ -30,9 +37,33 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            takePicuture()
+        }
 
 
 
+    }
+
+    private fun takePicuture(){
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            intent.resolveActivity(packageManager)?.also {
+                val time: String = SimpleDateFormat("yyyyMMdd_HHmmss")
+                        .format(Date())
+                val values = ContentValues().apply {
+                    put(MediaStore.Images.Media.DISPLAY_NAME, "${time}_.jpg")
+                    put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                }
+                val collection = MediaStore.Images.Media
+                        .getContentUri("external")
+                val photoUri = contentResolver.insert(collection, values)
+                photoUri?.let {
+                    currentPhotoUri = it
+                }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, REQUEST_PICTURE)
+            }
+        }
     }
 
     private fun preaview(){
@@ -49,7 +80,8 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        //データを受け取る
+        //=====================================データを受け取る ========================================
+        //画質悪いデータ
         if (requestCode == REQUEST_PREVIEW && data!=null && resultCode == RESULT_OK){
             //データの格納
             val imagebitmap = data?.extras?.get("data") as Bitmap
@@ -66,6 +98,31 @@ class MainActivity : AppCompatActivity() {
             //デコード
             val decodedByte: ByteArray = Base64.decode(output, 0)
             imageView?.setImageBitmap(BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size))
+
+        //フルデータ
+        }else if(requestCode == REQUEST_PICTURE){
+            when(resultCode){
+                RESULT_OK ->{
+
+                    //＝＝＝＝＝＝＝＝＝＝＝＝＝＝BASE６４＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+                    //エンコード
+                    //uri→Bitmapに変換
+                    val immagex: Bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), currentPhotoUri);
+                    val baos = ByteArrayOutputStream()
+                    immagex.compress(Bitmap.CompressFormat.PNG, 100, baos)
+                    val b: ByteArray = baos.toByteArray()
+                    val output = Base64.encodeToString(b, Base64.NO_WRAP)
+
+                    //デコード
+                    val decodedByte: ByteArray = Base64.decode(output, 0)
+                    imageView?.setImageBitmap(BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size))
+
+                }
+
+                else ->{
+                    contentResolver.delete(currentPhotoUri, null, null)
+                }
+            }
         }
     }
 
